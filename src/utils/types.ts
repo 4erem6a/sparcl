edee56e8ba6ae83b97724f0a2bedb040ac81fe8b
4erem6a <Sviridoff_E@mail.ru>
2019-11-27 00:46:10 +0300

    Update utility types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,7 @@
 import { Parser } from "../parsing/Parser";
 
-export type ParserResultType<P extends Parser<any>> = P extends Parser<infer R>
-  ? R
-  : unknown;
+export type ParserResultType<P> = P extends Parser<infer R> ? R : unknown;
 
-export type ParserSequenceResultType<P extends Parser<any>[]> = {
-  [K in keyof P]: P[K] extends Parser<infer R> ? R : unknown;
+export type ParserSequenceResultType<P> = {
+  [K in keyof P]: ParserResultType<P[K]>;
 };
