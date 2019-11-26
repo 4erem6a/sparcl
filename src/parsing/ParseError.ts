ef990e7bb67ee671d568e032bd90d0256e74fbcd
4erem6a <Sviridoff_E@mail.ru>
2019-11-27 02:03:19 +0300

    Add error pointers

--- src/parsing/ParseError.ts
+++ src/parsing/ParseError.ts
@@ -1,10 +1,34 @@
+import { InputStream } from "../input/InputStream";
 import { SourcePosition } from "../input/SourcePosition";
 
 export class ParseError extends Error {
+  public readonly line?: string;
+  public readonly position?: SourcePosition;
+
   public constructor(
-    public readonly position: SourcePosition,
-    public readonly shortMessage: string
+    public readonly shortMessage: string,
+    public readonly source?: InputStream
   ) {
-    super(`${shortMessage} at ${position.line}:${position.column}`);
+    super(
+      `${shortMessage}${
+        source
+          ? ` at ${source.sourcePosition.line}:${
+              source.sourcePosition.column
+            }${createPointer(source)}`
+          : ""
+      }`
+    );
+
+    if (source) {
+      this.line = source.currentLine;
+      this.position = source.sourcePosition;
+    }
   }
+}
+
+function createPointer(src: InputStream): string {
+  const errorLine = src.currentLine;
+  const errorPosition = src.sourcePosition.column;
+
+  return `\n${errorLine}\n${" ".repeat(errorPosition)}^`;
 }
