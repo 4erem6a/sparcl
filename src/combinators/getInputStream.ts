a0194f8513a2bea33eb00de4f89f2c0669344cea
4erem6a <Sviridoff_E@mail.ru>
2019-12-01 01:41:15 +0300

    Add getInputStream combinator

---
+++ src/combinators/getInputStream.ts
@@ -0,0 +1,15 @@
+import { Parser, createParser } from "../parsing/Parser";
+import { InputStream } from "../input/InputStream";
+import { complete } from "../parsing/ParserResult";
+
+export function getInputStream<T>(parser: Parser<T>) {
+  return createParser<[T, InputStream]>(src => {
+    const result = parser.parse(src);
+
+    if (result.isError) {
+      return result;
+    }
+
+    return complete([result.value, src]);
+  });
+}
