b5840ba3c322d05cb3d206de9a549e7983fcf1bd
4erem6a <Sviridoff_E@mail.ru>
2019-12-01 01:13:23 +0300

    Add takeAll combinator test

---
+++ test/combinators/takeAll.test.js
@@ -0,0 +1,7 @@
+const { takeAll, digit } = require("../..");
+
+test("takeAll combinator", () => {
+  const parser = takeAll(digit);
+
+  expect(parser).toMatchResult("1234", ["1", "2", "3", "4"]);
+});
