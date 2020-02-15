e38def6a4c0596aeecec33fbe6a5c014d728b0d2
4erem6a <Sviridoff_E@mail.ru>
2020-02-15 18:12:31 +0300

    Update either combinator

--- src/combinators/either.ts
+++ src/combinators/either.ts
@@ -1,24 +1,28 @@
-import { Parser, createParser } from "../parsing/Parser";
+import { Parser } from "../parsing/Parser";
 import { ParseError } from "../parsing/ParseError";
 import { complete, raise } from "../parsing/ParsingResult";
 import { ParserResultType } from "../utils/types";
 
-export function either<T extends Parser<any>[]>(...parsers: T) {
-  return createParser<
-    T[number] extends never ? undefined : ParserResultType<T[number]>
-  >(src => {
+type EitherResult<T extends Parser<unknown>[]> = T[number] extends never
+  ? undefined
+  : ParserResultType<T[number]>;
+
+export function either<T extends Parser<unknown>[]>(
+  ...parsers: T
+): Parser<EitherResult<T>> {
+  return new Parser<EitherResult<T>>(src => {
     let error: ParseError | undefined;
 
-    for (let parser of parsers) {
+    for (const parser of parsers) {
       const result = parser.parse(src);
 
       if (result.isError) {
         error = result.error;
       } else {
-        return complete(result.value);
+        return complete(result.value as EitherResult<T>);
       }
     }
 
-    return error ? raise(error) : complete(undefined);
+    return error ? raise(error) : complete(undefined as EitherResult<T>);
   });
 }
