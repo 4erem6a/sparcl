7276eed0e14cc6783198efd03ef432dccc1e1d06
4erem6a <Sviridoff_E@mail.ru>
2020-02-15 18:24:09 +0300

    Update optional combinator

--- src/combinators/optional.ts
+++ src/combinators/optional.ts
@@ -1,12 +1,12 @@
-import { Parser, createParser } from "../parsing/Parser";
+import { Parser } from "../parsing/Parser";
 import { complete } from "../parsing/ParsingResult";
 
-export function optional<T>(parser: Parser<T>) {
-  return createParser<T | undefined>(src => {
+export function optional<T>(parser: Parser<T>): Parser<T | undefined> {
+  return new Parser<T | undefined>(src => {
     const result = parser.parse(src);
 
     const value = result.isError ? undefined : result.value;
 
-    return complete(value as T | undefined);
+    return complete(value);
   });
 }
