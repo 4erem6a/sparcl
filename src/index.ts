cee8c5d14af13df832c798f283c7e638b6969d12
4erem6a <Sviridoff_E@mail.ru>
2020-02-15 20:10:30 +0300

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-export * from "./combinators/coroutine";
 export * from "./combinators/either";
 export * from "./combinators/optional";
 export * from "./combinators/separatedBy";
 export * from "./combinators/sequence";
-export * from "./combinators/takeAll";
-export * from "./combinators/takeOneOrMore";
+export * from "./combinators/noneOrMore";
+export * from "./combinators/oneOrMore";
 export * from "./combinators/lookAhead";
+export * from "./combinators/tap";
 
 export * from "./parsers/digit";
-export * from "./parsers/digits";
 export * from "./parsers/eof";
 export * from "./parsers/letter";
-export * from "./parsers/letters";
 export * from "./parsers/regexp";
+export * from "./parsers/source";
 export * from "./parsers/string";
-export * from "./parsers/tapSource";
 export * from "./parsers/whitespace";
 
 export * from "./parsing/ParseError";
