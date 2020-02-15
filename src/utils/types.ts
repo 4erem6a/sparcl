add3502025b8d9ab9ad79852d6070c5482a64873
4erem6a <Sviridoff_E@mail.ru>
2020-02-15 18:23:23 +0300

    Remove ParserSequenceResultType

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,3 @@
 import { Parser } from "../parsing/Parser";
 
 export type ParserResultType<P> = P extends Parser<infer R> ? R : unknown;
-
-export type ParserSequenceResultType<P> = {
-  [K in keyof P]: ParserResultType<P[K]>;
-};
