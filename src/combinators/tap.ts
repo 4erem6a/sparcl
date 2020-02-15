076bf4d9dc168eb40f8a5852af46e6f512488251
4erem6a <Sviridoff_E@mail.ru>
2020-02-15 18:47:10 +0300

    Add tap combinator

--- src/combinators/tap.ts
+++ src/combinators/tap.ts
@@ -1,12 +1,15 @@
 import { Parser } from "../parsing/Parser";
 import { ParsingResult } from "../parsing/ParsingResult";
+import { InputStream } from "@4erem6a/inputstream";
 
 export function tap<T>(
   parser: Parser<T>,
-  fn: (result: ParsingResult<T>) => void
+  fn: (result: ParsingResult<T>, source: InputStream) => void
 ): Parser<T> {
-  return parser.mapResult(result => {
-    fn(result);
+  return new Parser<T>(src => {
+    const result = parser.parse(src);
+
+    fn(result, src);
 
     return result;
   });
