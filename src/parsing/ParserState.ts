23801ccec9e40f0cb580ffdc327a0e5a0776e402
4erem6a <Sviridoff_E@mail.ru>
2020-02-28 02:10:15 +0300

    Update ParserState

--- src/parsing/ParserState.ts
+++ src/parsing/ParserState.ts
@@ -1,7 +1,25 @@
 import { InputStream } from "@4erem6a/inputstream";
 import { ParsingResult } from "./ParsingResult";
 
+type ParserStateResult<T> = T extends never ? undefined : ParsingResult<T>;
+
 export interface ParserState<T = never> {
   source: InputStream;
-  result: T extends never ? undefined : ParsingResult<T>;
+  result: ParserStateResult<T>;
+}
+
+export function createState(source: InputStream): ParserState;
+export function createState<T>(
+  source: InputStream,
+  result: ParserStateResult<T>
+): ParserState;
+
+export function createState<T>(
+  source: InputStream,
+  result?: ParserStateResult<T>
+): ParserState<T> {
+  return {
+    source,
+    result: result as ParserStateResult<T>
+  };
 }
