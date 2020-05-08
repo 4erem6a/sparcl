5e15d605b7f607c3f4f6deac3233a434473843a3
4erem6a <Sviridoff_E@mail.ru>
2020-05-09 00:24:58 +0300

    Add p utility template

---
+++ src/utils/p.ts
@@ -0,0 +1,11 @@
+import { Parser } from "../parsing/Parser";
+import { string } from "../parsers/basic/string";
+
+export function p(
+  template: TemplateStringsArray,
+  substitutions: never[]
+): Parser<string> {
+  const pattern = String.raw(template, substitutions);
+
+  return string(pattern);
+}
